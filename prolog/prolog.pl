626aee27836ed1fb5f56cbf2cf315212ae2bfa14
miguelvillagra <miguel.villagra@fpuna.edu.py>
2022-06-27 00:54:50 -0400

    Se realizo el script prolog

---
+++ prolog/prolog.pl
@@ -0,0 +1,13 @@
+% Tipo de productos a la venta.
+
+pastel('pastel_gourmet').
+pastel('pastel_helado').
+pastel('pastel_pequeno').
+pastel('pastel_mediano').
+
+pastel_economico('pastel_pequeno').
+pastel_economico('pastel_mediano').
+
+% Regla.
+producto_a_la_venta(X):- pastel(X).
+pastel_no_economico(X):- pastel(X),  not(pastel_economico(X)).
